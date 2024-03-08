98204600e4dc2d647b4e532c08558c72b3e83827
aryansainias <155607558+aryansainias@users.noreply.github.com>
2024-03-08 14:15:20 +0530

    Create FindMaxNumber.java

---
+++ FindMaxNumber.java
@@ -0,0 +1,10 @@
+public class maximumnumberfind {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 23;
+        int c=Math.max(a, b) ;                                     //maximum number will print by using math and max function
+        System.out.println(c);
+    }
+
+    
+}
